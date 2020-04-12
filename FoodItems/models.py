f309f83520c0576929f9cdaf6af855835484288e
lokesh <lokesh27dinu@gmail.com>
2020-04-13 01:40:23 +0530

    Models of orders,food added

--- FoodItems/models.py
+++ FoodItems/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from users.models import RestaurantProfile
+from users.models import RestaurantProfile,CustomerProfile
 from PIL import Image
 
 class FoodNames(models.Model):
@@ -9,6 +9,11 @@ class FoodNames(models.Model):
 
     def __str__(self):
         return f'FoodItem name : {self.food_name}'
+
+class FoodCategory(models.Model):
+    foodCategories = models.CharField(max_length=30)
+    restaurants = models.ManyToManyField(RestaurantProfile,symmetrical=True)
+    
     
 class FoodItemsDescription(models.Model):
     description = models.TextField(editable=True)
@@ -16,6 +21,7 @@ class FoodItemsDescription(models.Model):
     image = models.ImageField(default='default_food.jpg',upload_to='FoodItems/')
     restaurant = models.ForeignKey(RestaurantProfile,on_delete=models.CASCADE)
     food_name = models.ForeignKey(FoodNames,on_delete=models.CASCADE)
+    foodCategory = models.ForeignKey(FoodCategory,on_delete=models.CASCADE,default=0)
 
     def __str__(self):
         return f'Food Item: {self.food_name} {self.price} {self.restaurant}'
@@ -33,3 +39,27 @@ class FoodItemsDescription(models.Model):
         except:
             pass
 
+
+class OrdersDescription(models.Model):
+    dateTime = models.DateTimeField(auto_now_add=True)
+    Subtotal = models.DecimalField(max_digits=10,decimal_places=2)
+    TotalPrice = models.DecimalField(max_digits=10,decimal_places=2)
+    charges = models.DecimalField(max_digits=10,decimal_places=2)
+    received = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    class OrderStatusEnum(models.IntegerChoices):
+        PROCESSING = 1
+        CONFIRMED = 2
+        CANCELLED = 3
+        OUTFORDELIVEERY = 4
+        COMPLETED = 5
+    
+    status = models.IntegerField(choices=OrderStatusEnum.choices)
+    restaurant = models.ForeignKey(RestaurantProfile,on_delete=models.CASCADE)
+    customer = models.ForeignKey(CustomerProfile,on_delete=models.CASCADE)
+    
+class OrderFoodQuantity(models.Model):
+    orderId = models.ForeignKey(OrdersDescription,on_delete=models.CASCADE)
+    foodItemId = models.ForeignKey(FoodItemsDescription,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+        
